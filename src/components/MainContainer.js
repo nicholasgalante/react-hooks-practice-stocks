f461b97a830d0128076db6acc399ceb9a394fe0c
Nicholas Galante <nicholasgalante94@gmail.com>
2023-01-24 19:04:33 -0500

    Fetch stock data and render stock components

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
 
 function MainContainer() {
+  const [stocksArray, setStocksArray] = useState([]);
+
+  useEffect(()=>{
+    fetch("http://localhost:3001/stocks")
+    .then(res => res.json())
+    .then(data => setStocksArray(data))
+  },[]) //set dependencies
+
+  const filteredStocks = stocksArray.filter(stock => stock)
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer filteredStocks={filteredStocks}/>
         </div>
         <div className="col-4">
           <PortfolioContainer />
--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Stock from "./Stock";
 
-function StockContainer() {
+function StockContainer({filteredStocks}) {
+
+  const renderedStocks = filteredStocks.map(stock => {
+    return <Stock key={stock.id} stock={stock}/>
+  })
+  
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      {renderedStocks}
     </div>
   );
 }
